1b01fd172499acbe031f122097203fe2d99ab6b5
Kaylin98 <kaylinmaharaj@gmail.com>
2026-03-30 12:38:34 +0200

    feat: enhance level generation by storing chunk references and adding movement logic

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -5,15 +5,39 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] GameObject chunkPrefab;
     [SerializeField] int startingChunks = 12;
     [SerializeField] Transform chunkParent;
-
     [SerializeField] float chunkLength = 10f;
+    [SerializeField] float chunkMoveSpeed = 5f;
+
+    GameObject[] chunks = new GameObject[12];
     
     void Start()
+    {
+        SpawnChunks();
+    }
+
+    void Update()
+    {
+        MoveChunks();
+    }
+
+    private void SpawnChunks()
     {
         for (int i = 0; i < startingChunks; i++)
         {
             Vector3 chunkPosition = new Vector3(transform.position.x, transform.position.y, i * chunkLength);
-            Instantiate(chunkPrefab, chunkPosition, Quaternion.identity, chunkParent);
+            GameObject newChunk = Instantiate(chunkPrefab, chunkPosition, Quaternion.identity, chunkParent);
+            chunks[i] = newChunk;
+        }
+    }
+
+    void MoveChunks()
+    {
+        for (int i = 0; i < chunks.Length; i++)
+        {
+            if (chunks[i] != null)
+            {
+                chunks[i].transform.Translate(Vector3.back * (chunkMoveSpeed * Time.deltaTime));
+            }
         }
     }
 }
