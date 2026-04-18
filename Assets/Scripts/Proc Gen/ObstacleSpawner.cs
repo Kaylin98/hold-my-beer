86020afce168c466d807fa8d276ff1d04f8e1ad7
Kaylin98 <kaylinmaharaj@gmail.com>
2026-04-18 14:43:16 +0200

    feat: add new prefabs for Axe Warrior assets

--- Assets/Scripts/Proc Gen/ObstacleSpawner.cs
+++ Assets/Scripts/Proc Gen/ObstacleSpawner.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
@@ -12,6 +11,10 @@ public class ObstacleSpawner : MonoBehaviour
     [SerializeField] float obstacleSpawnInterval = 1f;
     [SerializeField] float minObstacleSpawnInterval = .2f;
     [SerializeField] float spawnWidth = 4f;
+
+    [Header("Physics Fix")]
+    [Tooltip("How hard to shoot the obstacle at the floor so it bounces in time")]
+    [SerializeField] float downwardSpawnForce = 15f; 
 
     void Start() 
     {
@@ -33,9 +36,20 @@ public class ObstacleSpawner : MonoBehaviour
         while (true)
         {
             GameObject obstacleToSpawn = obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)];
-            Vector3 spawnPosition = transform.position + new Vector3(Random.Range(-spawnWidth, spawnWidth), transform.position.y, transform.position.z);
+            
+            // FIX 1: Only add the random X offset. Do not add Y and Z again!
+            Vector3 spawnPosition = transform.position + new Vector3(Random.Range(-spawnWidth, spawnWidth), 0, 0);
+            
             yield return new WaitForSeconds(obstacleSpawnInterval);
-            Instantiate(obstacleToSpawn, spawnPosition, Random.rotation, obstacleParent);
+            
+            GameObject spawnedObstacle = Instantiate(obstacleToSpawn, spawnPosition, Random.rotation, obstacleParent);
+
+            // FIX 2: Shoot it downwards so it hits the floor and bounces quickly
+            Rigidbody rb = spawnedObstacle.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddForce(Vector3.down * downwardSpawnForce, ForceMode.Impulse);
+            }
         }
     }
 }
